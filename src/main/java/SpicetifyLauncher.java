8de3cc68c612b545206ddcc674f9d3240356df88
Seraphin Berger <MCmoderSD@web.de>
2023-01-14 17:42:17 +0100

    Cleanup, Performance Improvement

--- src/main/java/SpicetifyLauncher.java
+++ src/main/java/SpicetifyLauncher.java
@@ -1,6 +1,21 @@
+import java.io.IOException;
+
 public class SpicetifyLauncher {
     public static void main(String[] args) {
-     new Upgrade();
-     new Initialization();
+        String[] commands = {"powershell.exe spicetify.exe upgrade", "powershell.exe spicetify.exe apply"};
+        for (int i = 0; i < commands.length; i++) {
+            String command = commands[i];
+            Process powerShellProcess;
+            try {
+                powerShellProcess = Runtime.getRuntime().exec(command);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                powerShellProcess.getOutputStream().close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
